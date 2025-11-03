c5fa8e5628391ee95737cca85d75c0d431bb3932
ayaan-513 <ayaan.hussain0513@gmail.com>
2025-11-03 02:02:39 -0500

    done

---
+++ 2DX3FINALPROJECT/FinalProjectDeliverableMicroControllerCode/Objects/studio4b.d
@@ -0,0 +1,5 @@
+./objects/studio4b.o: \
+  ..\..\ProjectDeliverableOne\Studio4B_Template\ (1)\Studio4B.c \
+  ..\..\ProjectDeliverableOne\Studio4B_Template\ (1)\tm4c1294ncpdt.h \
+  ..\..\ProjectDeliverableOne\Studio4B_Template\ (1)\PLL.h \
+  ..\..\ProjectDeliverableOne\Studio4B_Template\ (1)\SysTick.h
